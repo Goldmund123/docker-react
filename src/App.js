0876ae23b57844446a15dc6f252f2e9ff0990125
Jay <jay@Charmaines-MacBook-Air.local>
2020-03-29 11:43:30 -0700

    added travis file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Woo
+          How are you doing?
         </p>
         <a
           className="App-link"
